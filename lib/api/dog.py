daa7b7e9bf2a360f57e7c955fcc405aab831257f
Antonida <43772120+Rainbow42@users.noreply.github.com>
2020-07-13 12:15:09 +0400

    Update dog.py

--- lib/api/dog.py
+++ lib/api/dog.py
@@ -1,24 +1,27 @@
 import requests
-import main
+from antonidabotternovnik.bin.main import send_message
 import re
 import json
-from main import send_message
+
 
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()
     url = contents['url']
 
     return url
+
+
 def get_image_url():
-    allowed_extension = ['jpg','jpeg','png']
+    allowed_extension = ['jpg', 'jpeg', 'png']
     file_extension = ''
     while file_extension not in allowed_extension:
         url = get_url()
-        file_extension = re.search("([^.]*)$",url).group(1).lower()
+        file_extension = re.search("([^.]*)$", url).group(1).lower()
         """with open('updets.json', 'w')as f:
             json.dump(url, f)"""
     return url
 
+
 def bots(chat_id):
     url = get_image_url()
-    send_message(chat_id,url)
+    send_message
